ed5eaea80a5e719b145d3e87901240acccddd84b
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-02-28 22:41:52 +0300

    add reinstall vue.js

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,8 +1,28 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
+import HomePage from './components/HomePage.vue'
+import AboutPage from './components/AboutPage.vue'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+
+// Routes registration
+const routes: RouteRecordRaw[] = [
+    { path: '/', component: HomePage },
+    { path: '/about', component: AboutPage },
+]
+
+// Create Router
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+// Create app
+createApp(App) // Root Component
+    .use(router) // Use Router
+    .mount('#app') // Html root element
 
 const app = createApp(App)
 
